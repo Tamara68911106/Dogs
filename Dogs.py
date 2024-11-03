947fe30b7a61273376e65eb8a8f45e42936f2ac8
User <toma2006.06.11.@gmail.com>
2024-11-04 00:53:42 +0400

    Добавлен прогресс-бар

--- Dogs.py
+++ Dogs.py
@@ -1,8 +1,9 @@
 from tkinter import*
+from tkinter import ttk
 from tkinter import messagebox as mb
 import requests # что загрузать из инета изображения
 from PIL import Image, ImageTk  # чтобы обрабатывать
-from io import BytesIo # чтобы обрабатывать картинку
+from io import BytesIO # чтобы обрабатывать картинку
 
 
 def get_dog_image():
@@ -10,7 +11,7 @@ def get_dog_image():
         response= requests.get("https://dog.ceo/api/breeds/image/random")
         response.raise_for_status()
         data = response.json()
-        return data('message')
+        return data ['message']
     except Exception  as e:
         mb.showerror("Ошибка", f"Возникла ошибка при запросе к API {e}")
         return None # если ошибка  - возвращаем пустоту(None)
@@ -21,7 +22,7 @@ def show_image():
         try:
             response = requests.get(image_url, stream=True)
             response.raise_for_status()
-            img_data = BytesIo(response.content)# с помощью байт загружаем респонсконт
+            img_data = BytesIO(response.content)# с помощью байт загружаем респонсконт
             img = Image.open(img_data)
             img.thumbnail((300,300))
             img = ImageTk.PhotoImage(img)
@@ -30,14 +31,26 @@ def show_image():
         except Exception as e:
             mb.showerror("Ошибка", f"Возникла ошибка при загрузке изображения {e}")# если что-то пойдет не так - выдаст ошибку
 
+    progress.stop()
+
+def prog():  # строка, отображающая загрузку
+    progress['value']=0
+    progress.start(30)
+    window.after(3000, show_image)
+
 window = Tk()
 window.title("Картинки с собачками")
-window.geomety("360x420")
+window.geometry("360x420")
 
 
-label = Label()
+label = ttk.Label()
 label.pack(pady=10)
 
 
-button = Button(text="загрузить изогражения", command=show_image)
+button =ttk.Button(text="загрузить изображение", command=prog)
 button.pack(pady=10)
+
+progress = ttk.Progressbar(mode="determinate", length=300)
+progress.pack(pady=10)
+
+window.mainloop()
